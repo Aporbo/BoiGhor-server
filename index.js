ea2e21398288b01c7c1303fc01e15a1dfb371fbf
Aporbo <aporbo.ghosh@northsouth.edu>
2023-06-09 16:08:35 +0600

    changed database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4pnvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://aporbo:<aporbo1121999>@cluster0.4shxpnj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
